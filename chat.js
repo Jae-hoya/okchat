51b8138509b4a6a17b837483f4e0d58182d5677b
Jaeho <74150155+Jae-hoya@users.noreply.github.com>
2026-04-19 18:04:07 +0900

    client

---
+++ chat.js
@@ -0,0 +1,21 @@
+var socket = io();
+var messages = document.getElementById('messages');
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  if (input.value) {
+    socket.emit('chatmessage', input.value);
+    input.value = '';
+  }
+});
+
+socket.on('chatmessage', function (msg) {
+  var item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+
+  window.scrollTo(0, document.body.scrollHeight);
+});
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// 서버 코드
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
